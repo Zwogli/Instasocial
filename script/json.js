febb25b8df6829c498bcb9a1fe36acd35c7d7fa9
Zwogli <Mathiaskohler@web.de>
2023-03-02 12:33:06 +0100

    add profil

---
+++ script/json.js
@@ -0,0 +1,72 @@
+let profil = [
+    {
+        "profil-img": `../img/profil/profileMathiasKohler.jpg`,
+        "profil-username": "Zwogli",
+        "profil-name": "Mathias Kohler",
+    }
+]
+let posts = [
+    {
+        "user-img": `../img/user-followed/bart-g219181065_640.png`,
+        "author" : "El Barto",
+        "location": "742 Evergreen Terrace, Sprinfield, USA",
+        "content": `../img/spray-can-gfda02e312_640.jpg`,
+        "likes": 666,
+        "message": "Wieder einmal hat El Barto die Nachbarschaft aufgehübscht!",
+        "comment": [],
+    },
+    {
+        "user-img": `../img/user-followed/bird-gc5be3399e_640.png`,
+        "author" :  "A. Dumbledore",
+        "location": "Hogwarts School of Witchcraft and Wizardry, Dufftown, Scotland",
+        "content": `../img/pexels-joshua-roberts-13694285.jpg`,
+        "likes": 1300,
+        "message": "Unserem Wildhüter Hagrid ist sein Haustier ausgebüchst. Wer Norberta sieht keine Angst Sie ist harmlos.",
+        "comment": [],
+    },
+    {
+        "user-img": `../img/user-followed/lord-who-rings-gd1cfe2b71_640.png`,
+        "author" :  "Gandalf",
+        "location": "Hobbingen, Auenland, Middle Earth",
+        "content": `../img/middle-earth-gb2bb08e64_640.jpg`,
+        "likes": 15360,
+        "message": "Nach den erschütternden Ereignissen in Mittelerde, setzt die Tourismusbranche erste anzeichen für den Wiederaufbau.",
+        "comment": [],
+    },
+    {
+        "user-img": `../img/user-followed/news-gdb05a2f9f_640.png`,
+        "author" :  "News",
+        "location": "Norwegen, Niedersachsen, Germany",
+        "content": `../img/pexels-dominika-roseclay-1094794.jpg`,
+        "likes": 500000,
+        "message": "John Doe erzielte seinen neusten Rekord beim Freeclimbing in der Uckermarck auf der Hagelberg mit 200,2m höhe",
+        "comment": [],
+    },
+]
+let newUser = [
+    {
+        "user-img":`../img/user-new/pexels-ekaterina-bolovtsova-6607387.png`,
+        "user-name": "KatJobs",
+        "name": "Katie Jakobs",
+    },
+    {
+        "user-img":`../img/user-new/pexels-guilherme-almeida-1858175.png`,
+        "user-name": "JaneDoe",
+        "name": "Jane Dorothy",
+    },
+    {
+        "user-img":`../img/user-new/pexels-justin-shaifer-1222271.png`,
+        "user-name": "RealJustin",
+        "name": "Justin Hamilton",
+    },
+    {
+        "user-img":`../img/user-new/pexels-ola-dapo-3345882.png`,
+        "user-name": "TelePete",
+        "name":"Peter Blackson",
+    },
+    {
+        "user-img":`../img/user-new/pexels-quyn-phạm-13803421.png`,
+        "user-name": "animeGirl_2032023",
+        "name": "Quyn Mi Sao",
+    }
+]
