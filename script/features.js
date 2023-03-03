d75a227c579fbf574a5089cf3b9114c83c9e1175
Zwogli <Mathiaskohler@web.de>
2023-03-03 12:50:54 +0100

    edit darkmode

--- script/features.js
+++ script/features.js
@@ -1,4 +1,15 @@
 function darkMode(){
     let body = document.getElementById('body');
+    let img = document.getElementById('img-darkmode');
     body.classList.toggle("darkMode");
+
+        if(body.classList =="darkMode"){
+            img.src = `img/svg/light_mode_FILL0_wght400_GRAD0_opsz48.svg`;
+        }else{
+            img.src = `img/svg/dark_mode_FILL0_wght400_GRAD0_opsz48.svg`;
+        }
+}
+
+function menu(){
+
 }
